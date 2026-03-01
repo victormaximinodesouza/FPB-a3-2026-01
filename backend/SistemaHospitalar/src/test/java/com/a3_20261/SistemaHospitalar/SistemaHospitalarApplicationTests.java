939e978622c0b56f73cf6c202b789e5c170539c0
Emanuel Viktor <emanuel.bol.cco@gmail.com>
2026-03-01 20:03:37 -0300

    Adicionando Spring configuração inicial do projeto

---
+++ backend/SistemaHospitalar/src/test/java/com/a3_20261/SistemaHospitalar/SistemaHospitalarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.a3_20261.SistemaHospitalar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaHospitalarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
