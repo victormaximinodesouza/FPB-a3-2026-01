24bcb0428a9a299b51e3f22fcb763b5173cef6a1
Emanuel Viktor <emanuel.bol.cco@gmail.com>
2026-03-05 18:29:57 -0300

    Paciente resource

--- backend/SistemaHospitalar/src/main/java/com/a3_20261/SistemaHospitalar/entities/Paciente.java
+++ backend/SistemaHospitalar/src/main/java/com/a3_20261/SistemaHospitalar/entities/Paciente.java
@@ -1,9 +1,13 @@
 package entities;
 
+import java.io.Serial;
+import java.io.Serializable;
 import java.util.Date;
 import java.util.Objects;
 
-public class Paciente {
+public class Paciente implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
 
     private Integer id;
     private String nome;
---
+++ backend/SistemaHospitalar/src/main/java/com/a3_20261/SistemaHospitalar/resources/PacienteResources.java
@@ -0,0 +1,21 @@
+package resources;
+
+import entities.Paciente;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+
+@RestController
+@RequestMapping(value ="/pacientes")
+public class PacienteResources {
+
+    @GetMapping
+    public ResponseEntity<Paciente> findAll(){
+        Paciente p = new Paciente(1,"maria",158558509,null,"rua maria sant","20","maria@gmail.com");
+                return ResponseEntity.ok().body(p);
+
+    }
+}
