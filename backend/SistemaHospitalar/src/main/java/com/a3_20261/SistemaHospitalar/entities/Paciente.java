ac7a504f58892e8bba5533d24d437bcf92042d7f
Emanuel Viktor <emanuel.bol.cco@gmail.com>
2026-03-09 19:32:33 -0300

    Medico entities config

--- backend/SistemaHospitalar/src/main/java/com/a3_20261/SistemaHospitalar/entities/Paciente.java
+++ backend/SistemaHospitalar/src/main/java/com/a3_20261/SistemaHospitalar/entities/Paciente.java
@@ -7,7 +7,7 @@ import java.io.Serializable;
 import java.util.Date;
 import java.util.Objects;
 @Entity
-@Table(name = "tb_user")
+@Table(name = "tb_paciente")
 public class Paciente implements Serializable {
     @Serial
     private static final long serialVersionUID = 1L;
