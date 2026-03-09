f4af6f3f4735c8eb924d049de99b0bcca75d0879
Emanuel Viktor <emanuel.bol.cco@gmail.com>
2026-03-09 19:16:03 -0300

    TestConfig

---
+++ backend/SistemaHospitalar/src/main/java/com/a3_20261/SistemaHospitalar/Repository/MedicoRepository.java
@@ -0,0 +1,7 @@
+package com.a3_20261.SistemaHospitalar.Repository;
+
+import com.a3_20261.SistemaHospitalar.entities.Medico;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MedicoRepository extends JpaRepository<Medico,Integer> {
+}
---
+++ backend/SistemaHospitalar/src/main/java/com/a3_20261/SistemaHospitalar/config/TestConfig.java
@@ -0,0 +1,27 @@
+package com.a3_20261.SistemaHospitalar.config;
+
+import com.a3_20261.SistemaHospitalar.Repository.MedicoRepository;
+import com.a3_20261.SistemaHospitalar.entities.Medico;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import java.util.Arrays;
+
+@Configuration
+@Profile("test")
+public class TestConfig implements CommandLineRunner {
+    @Autowired
+    private MedicoRepository medicoRepository;
+
+
+    @Override
+    public void run(String... args) throws Exception {
+        
+        Medico m1 = new Medico(null,"joao",3232,"cabeca",922992922);
+        Medico m2 = new Medico(null,"maria",32333,"pe",992922);
+
+        medicoRepository.saveAll(Arrays.asList(m1,m2));
+    }
+}
