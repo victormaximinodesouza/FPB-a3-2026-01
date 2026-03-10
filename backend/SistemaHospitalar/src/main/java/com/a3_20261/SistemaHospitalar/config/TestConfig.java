86efaf63bd81ad44dbd2653724eee7bc35ab4038
Emanuel Viktor <emanuel.bol.cco@gmail.com>
2026-03-10 18:49:27 -0300

    Atendimento teste

--- backend/SistemaHospitalar/src/main/java/com/a3_20261/SistemaHospitalar/config/TestConfig.java
+++ backend/SistemaHospitalar/src/main/java/com/a3_20261/SistemaHospitalar/config/TestConfig.java
@@ -1,6 +1,8 @@
 package com.a3_20261.SistemaHospitalar.config;
 
+import com.a3_20261.SistemaHospitalar.Repository.AtendimentoRepository;
 import com.a3_20261.SistemaHospitalar.Repository.MedicoRepository;
+import com.a3_20261.SistemaHospitalar.entities.Atendimento;
 import com.a3_20261.SistemaHospitalar.entities.Medico;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -15,13 +17,31 @@ public class TestConfig implements CommandLineRunner {
     @Autowired
     private MedicoRepository medicoRepository;
 
+    @Autowired
+    private AtendimentoRepository atendimentoRepository;
 
     @Override
     public void run(String... args) throws Exception {
-        
-        Medico m1 = new Medico(null,"joao",3232,"cabeca",922992922);
-        Medico m2 = new Medico(null,"maria",32333,"pe",992922);
 
-        medicoRepository.saveAll(Arrays.asList(m1,m2));
+        Medico m1 = new Medico(null, "joao", 3232, "cabeca", 922992922);
+        Medico m2 = new Medico(null, "maria", 32333, "pe", 992922);
+
+        medicoRepository.saveAll(Arrays.asList(m1, m2));
+
+
+        Atendimento a1 = new Atendimento(null, "Prioridade", null);
+        Atendimento a2 = new Atendimento(null, "Prioridade", null);
+
+        atendimentoRepository.saveAll(Arrays.asList(a1, a2));
     }
-}
+
+
+
+
+    }
+
+
+
+
+
+
